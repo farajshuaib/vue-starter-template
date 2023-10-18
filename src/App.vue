59d957a5a82d0b779d2dcfc0339c7b708690dcc9
f.shuaib <f.shuaib@masarat.ly>
2023-10-18 15:23:09 +0200

    clear nad ifxies

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { onMounted } from "vue";
 import { RouterView } from "vue-router";
 import { setLocale } from "./core/i18n";
-import { AppLocales } from "./core/enums";
+import { AppLocales } from "./core/constant";
 
 onMounted(() => {
   setLocale(
